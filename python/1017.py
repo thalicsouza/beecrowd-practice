de4bebeae9835d57fe8f546f92dbc1297144a1d3
thalistarr <thalita.souza@gamersclub.com.br>
2022-06-20 21:49:54 -0300

    exercises 1017 to 1018

---
+++ python/1017.py
@@ -0,0 +1,7 @@
+# 1017 - Fuel Spent
+# original link: https://www.beecrowd.com.br/judge/en/problems/view/1017
+
+time = int(input())
+speed = int(input())
+distance = time * speed
+print("{:.3f}".format(distance/12))
---
+++ python/1018.py
@@ -0,0 +1,23 @@
+value = int(input())
+print(value)
+
+print(f"{int(value / 100)} nota(s) de R$ 100,00")
+value = value % 100
+    
+print(f"{int(value / 50)} nota(s) de R$ 50,00")
+value = value % 50
+    
+print(f"{int(value / 20)} nota(s) de R$ 20,00")
+value = value % 20
+
+print(f"{int(value / 10)} nota(s) de R$ 10,00")
+value = value % 10
+    
+print(f"{int(value / 5)} nota(s) de R$ 5,00")
+value = value % 5
+    
+print(f"{int(value / 2)} nota(s) de R$ 2,00")
+value = value % 2
+    
+print(f"{int(value / 1)} nota(s) de R$ 1,00")
+value = value % 1
